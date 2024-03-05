d50f882f550d250ec55fce331101903d93d69978
mariosknl <mariosknl@outlook.com>
2024-03-05 10:39:40 +0200

    fetch movie details

--- api/movies.ts
+++ api/movies.ts
@@ -1,13 +1,15 @@
+const headers = {
+	accept: "application/json",
+	Authorization:
+		"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmY2I4MzlkNzdhNmI1MzZjYWU3YzlmOTRkNDI2NGU4NiIsInN1YiI6IjYxYWZhNGRlZTI2M2JiMDA5MWRkMjYyNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OjqLyPwrPtd-WxGrOwDNkxV-9dF4irCfrZoF0WDUwbc",
+};
+
 export const fetchTopRatedMovies = async () => {
 	const url =
 		"https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
 	const options = {
 		method: "GET",
-		headers: {
-			accept: "application/json",
-			Authorization:
-				"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmY2I4MzlkNzdhNmI1MzZjYWU3YzlmOTRkNDI2NGU4NiIsInN1YiI6IjYxYWZhNGRlZTI2M2JiMDA5MWRkMjYyNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OjqLyPwrPtd-WxGrOwDNkxV-9dF4irCfrZoF0WDUwbc",
-		},
+		headers,
 	};
 
 	const res = await fetch(url, options);
@@ -18,4 +20,21 @@ export const fetchTopRatedMovies = async () => {
 
 	const json = await res.json();
 	return json.results;
+};
+
+export const fetchMovie = async (id: number) => {
+	const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+	const options = {
+		method: "GET",
+		headers,
+	};
+
+	const res = await fetch(url, options);
+
+	if (!res.ok) {
+		throw new Error("Failed to details for the selected movie");
+	}
+
+	const json = await res.json();
+	return json;
 };
--- app/[id].tsx
+++ app/[id].tsx
@@ -1,12 +1,31 @@
+import { fetchMovie } from "@/api/movies";
+import { useQuery } from "@tanstack/react-query";
 import { useLocalSearchParams } from "expo-router";
-import { View, Text } from "react-native";
+import { ActivityIndicator, Text, View } from "react-native";
 
 const MovieDetails = () => {
 	const { id } = useLocalSearchParams();
 
+	const {
+		data: movie,
+		isLoading,
+		error,
+	} = useQuery({
+		queryKey: ["movie", id],
+		queryFn: () => fetchMovie(+id),
+	});
+
+	if (isLoading) {
+		return <ActivityIndicator />;
+	}
+
+	if (error) {
+		return <Text>{error.message}</Text>;
+	}
+
 	return (
 		<View>
-			<Text>MovieDetails - {id}</Text>
+			<Text style={{ fontSize: 24, fontWeight: "500" }}>{movie?.title}</Text>
 		</View>
 	);
 };
