ec716f459abc47ae8daa8039137d9c3b93946392
mariosknl <mariosknl@outlook.com>
2024-03-05 11:58:37 +0200

    mutation

---
+++ api/watchlist.ts
@@ -0,0 +1,26 @@
+export const addMovieToWatchList = async (movieId: number) => {
+	const url = "https://api.themoviedb.org/3/account/11546205/watchlist";
+	const options = {
+		method: "POST",
+		headers: {
+			accept: "application/json",
+			"content-type": "application/json",
+			Authorization:
+				"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmY2I4MzlkNzdhNmI1MzZjYWU3YzlmOTRkNDI2NGU4NiIsInN1YiI6IjYxYWZhNGRlZTI2M2JiMDA5MWRkMjYyNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OjqLyPwrPtd-WxGrOwDNkxV-9dF4irCfrZoF0WDUwbc",
+		},
+		body: JSON.stringify({
+			media_type: "movie",
+			media_id: movieId,
+			watchlist: true,
+		}),
+	};
+
+	const res = await fetch(url, options);
+
+	if (!res.ok) {
+		throw new Error("Failed to details for the selected movie");
+	}
+
+	const json = await res.json();
+	return json;
+};
--- app/[id].tsx
+++ app/[id].tsx
@@ -1,7 +1,9 @@
 import { fetchMovie } from "@/api/movies";
-import { useQuery } from "@tanstack/react-query";
+import { addMovieToWatchList } from "@/api/watchlist";
+import { FontAwesome } from "@expo/vector-icons";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import { Stack, useLocalSearchParams } from "expo-router";
-import { ActivityIndicator, Image, Text, View } from "react-native";
+import { ActivityIndicator, Image, Pressable, Text, View } from "react-native";
 
 const MovieDetails = () => {
 	const { id } = useLocalSearchParams();
@@ -13,6 +15,10 @@ const MovieDetails = () => {
 	} = useQuery({
 		queryKey: ["movie", id],
 		queryFn: () => fetchMovie(+id),
+	});
+
+	const { mutate } = useMutation({
+		mutationFn: () => addMovieToWatchList(+id),
 	});
 
 	if (isLoading) {
@@ -36,6 +42,15 @@ const MovieDetails = () => {
 				<Text style={{ fontSize: 30, fontWeight: "500", marginVertical: 10 }}>
 					{movie?.title}
 				</Text>
+				<View style={{ marginVertical: 10 }}>
+					<Pressable
+						onPress={() => mutate()}
+						style={{ flexDirection: "row", alignItems: "center", gap: 5 }}
+					>
+						<FontAwesome name="bookmark-o" size={24} color="black" />
+						<Text>Add to watchlist</Text>
+					</Pressable>
+				</View>
 				<Text style={{ fontSize: 16 }}>{movie?.overview}</Text>
 			</View>
 		</View>
