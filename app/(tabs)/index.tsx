cb0e8f2b04f4528283389da0e260168afcd2af7c
mariosknl <mariosknl@outlook.com>
2024-03-05 10:03:48 +0200

    implementing tanstack/react-query

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,30 +4,26 @@ import { View } from "@/components/Themed";
 import { useEffect, useState } from "react";
 import { fetchTopRatedMovies } from "@/api/movies";
 
+import { useQuery } from "@tanstack/react-query";
+
 export default function TabOneScreen() {
-	const [movies, setMovies] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-
-	useEffect(() => {
-		const fetchMovies = async () => {
-			setIsLoading(true);
-
-			const movies = await fetchTopRatedMovies();
-			setMovies(movies);
-
-			setIsLoading(false);
-		};
-		fetchMovies();
-	}, []);
+	const { data, isLoading, error } = useQuery({
+		queryKey: ["movies"],
+		queryFn: fetchTopRatedMovies,
+	});
 
 	if (isLoading) {
 		return <ActivityIndicator />;
 	}
 
+	if (error) {
+		return <Text>{error.message}</Text>;
+	}
+
 	return (
 		<View style={styles.container}>
 			<FlatList
-				data={movies}
+				data={data}
 				renderItem={({ item }) => (
 					<View>
 						<Text>{item.title}</Text>
